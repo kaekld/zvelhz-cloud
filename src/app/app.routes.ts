14d294f5d4f773e989293295a3d54d1060e901d5
kaekld <kledmonroy@gmail.com>
2025-12-10 11:10:19 -0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login', loadComponent: () => {
+      return import('./auth/pages/login-screen/login-screen').then(m => m.LoginScreen)
+    }
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
