d626993d9f45c38bdc5c0c39feb6355b70bde9c7
kaekld <kledmonroy@gmail.com>
2025-12-11 02:13:04 -0600

    some changes

--- src/app/home/components/navbar/navbar.ts
+++ src/app/home/components/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LoginService } from 'src/app/auth/services/login-service';
+import { UserFilesService } from '../../services/user-files-service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,16 @@ import { LoginService } from 'src/app/auth/services/login-service';
 })
 export class Navbar {
   private readonly loginService = inject(LoginService);
+  private readonly router = inject(Router)
+  private readonly userFilesService = inject(UserFilesService)
+
   public userName = signal<string>(this.loginService.userdata()?.nombre ?? 'Jhon Doe');
+
+  onLogout(): void {
+    this.router.navigate(['/login'])
+    this.loginService.userdata.set(null);
+    this.userFilesService.userFileList.set([])
+  }
+
 
 }
